c976bcbb56c90dfcc4c4a37edf80e541d223960a
Timo <38291523+lovetodream@users.noreply.github.com>
2022-06-05 20:44:51 +0200

    fix: listen on all interfaces

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ fastify.post('/subscribe', async (request, reply) => {
 
 const start = async () => {
     try {
-        await fastify.listen(4000);
+        await fastify.listen(4000, '0.0.0.0');
         fastify.log.info(`server listening on ${fastify.server.address().port}`);
     } catch (err) {
         fastify.log.error(err);
